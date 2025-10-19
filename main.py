a64593cc77ed56a208a13d768b32b4b5a0e308cf
Patr4ck <mukhammadislamma@gmail.com>
2025-10-19 16:25:15 -0500

    Davikrisa

--- main.py
+++ main.py
@@ -10,18 +10,6 @@ def index():
 def home():
     return "home Page"
 
-@app.route("/about")
-def about():
-    return "Hi This is the Test Site"
-
-@app.route("/bet")
-def bet():
-    return "let's bet on your luck"
-
-@app.route("/net")
-def net():
-    return "network is the key"
-
 
 if __name__=="__main__":
     app.run(debug=True)
